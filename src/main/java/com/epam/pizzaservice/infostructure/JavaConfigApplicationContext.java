76b2a70b0fcdd7f685f88fae22897ec942bc2546
Konstiantyn <kostya.kobzev@gmail.com>
2015-07-24 16:03:38 +0300

    DI go to IoC

--- src/main/java/com/epam/pizzaservice/infostructure/JavaConfigApplicationContext.java
+++ src/main/java/com/epam/pizzaservice/infostructure/JavaConfigApplicationContext.java
@@ -1,9 +1,13 @@
 package com.epam.pizzaservice.infostructure;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaConfigApplicationContext implements ApplicationContext {
 	private Config config;
+	private final Map<String, Object> beans = new HashMap<>();
 
 	public JavaConfigApplicationContext(Config config) {
 		this.config = config;
@@ -11,14 +15,30 @@ public class JavaConfigApplicationContext implements ApplicationContext {
 
 	@Override
 	public Object getBean(String beanName) throws Exception{
-		Class<?> clazz = config.getImplementation(beanName);
+		Object bean = beans.get(beanName);
+		
+		if (bean != null) {
+			return bean;
+		}
+		
+		BeanBuilder beanBuilder = new BeanBuilder(beanName);
+		beanBuilder.createObject();
+		beanBuilder.createProxy();
+		beanBuilder.callInitMethod();
+		
+		return beanBuilder.getObject();
+		
+		
+		/*Class<?> clazz = config.getImplementation(beanName);
 		
 		Constructor<?> constructor = clazz.getConstructors()[0];
 		//Parameter[] parameters = constructor.getParameters();
 		Class<?>[] parameters = constructor.getParameterTypes();
 		
 		if (parameters.length == 0) {
-			return clazz.newInstance();
+			bean = clazz.newInstance();
+			beans.put(beanName, bean);
+			return bean;
 		}
 		
 		Object[] constructorParameters = new Object[parameters.length];
@@ -30,7 +50,62 @@ public class JavaConfigApplicationContext implements ApplicationContext {
 			constructorParameters[i] = getBean(className);
 		}
 
-		return constructor.newInstance(constructorParameters);
+		bean = constructor.newInstance(constructorParameters);
+		beans.put(beanName, bean);
+		return bean;*/
+	}
+	
+	class BeanBuilder{
+		private Object obj;
+		private String beanName;
+				
+		public BeanBuilder(String beanName) {
+			this.beanName = beanName;
+		}
+
+		public void createObject() throws Exception{
+			Class<?> clazz = config.getImplementation(beanName);
+			
+			Constructor<?> constructor = clazz.getConstructors()[0];
+			//Parameter[] parameters = constructor.getParameters();
+			Class<?>[] parameters = constructor.getParameterTypes();
+			
+			if (parameters.length == 0) {
+				obj = clazz.newInstance();
+				beans.put(beanName, obj);
+				return;
+			}
+			
+			Object[] constructorParameters = new Object[parameters.length];
+			
+			for (int i=0; i<parameters.length; i++) {
+				String className = parameters[i].getSimpleName();
+				className = className.substring(0, 1).toLowerCase() + className.substring(1);
+				
+				constructorParameters[i] = getBean(className);
+			}
+
+			obj = constructor.newInstance(constructorParameters);
+			beans.put(beanName, obj);
+		}	
+		
+		public void callInitMethod() throws Exception{
+			Class<?> clazz = obj.getClass();
+			Method method;
+			try {
+				method = clazz.getMethod("init");
+			} catch (NoSuchMethodException e) {
+				return;
+			}
+			
+			method.invoke(obj);
+		}
+		
+		public void createProxy(){}
+		
+		public Object getObject(){
+			return obj;
+		}
 	}
 
 }
--- src/main/java/com/epam/pizzaservice/repository/TestPizzaRepository.java
+++ src/main/java/com/epam/pizzaservice/repository/TestPizzaRepository.java
@@ -7,22 +7,19 @@ import com.epam.pizzaservice.domain.Pizza;
 import com.epam.pizzaservice.domain.PizzaType;
 
 public class TestPizzaRepository implements PizzaRepository {
-	private static List<Pizza> staticListOfPizza;
+	private List<Pizza> listOfPizza;
 	
-	static{
-		staticListOfPizza = new ArrayList<Pizza>();
-		staticListOfPizza.add(new Pizza(1, "VEGETERIAN", 55.30, PizzaType.VEGETERIAN));
-		staticListOfPizza.add(new Pizza(2, "SEA", 68.30, PizzaType.SEA));
-		staticListOfPizza.add(new Pizza(3, "MEAT", 65.30, PizzaType.MEAT));		
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.epam.pizzaservice.repository.PizzaRepository#getPizzaByID(java.lang.Integer)
-	 */
 	@Override
 	public Pizza getPizzaByID(Integer id) {
-		for (Pizza pizza : staticListOfPizza) if(pizza.getId().equals(id)) return pizza;
+		for (Pizza pizza : listOfPizza) if(pizza.getId().equals(id)) return pizza;
 		return null;
+	}
+	
+	public void init(){
+		listOfPizza = new ArrayList<Pizza>();
+		listOfPizza.add(new Pizza(1, "VEGETERIAN", 55.30, PizzaType.VEGETERIAN));
+		listOfPizza.add(new Pizza(2, "SEA", 68.30, PizzaType.SEA));
+		listOfPizza.add(new Pizza(3, "MEAT", 65.30, PizzaType.MEAT));
 	}
 
 }
